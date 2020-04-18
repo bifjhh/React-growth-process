e8309c27a537fff72b7994660e6b6114cd41e977
sunkun <929160004@163.com>
2020-04-18 16:58:08 +0800

    feat: 组件生命周期--卸载时componentWillUnmount

--- demo_03_component/src/LifeCycle.jsx
+++ demo_03_component/src/LifeCycle.jsx
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+class Counter extends Component {
+  render() {
+    return (
+      <div>num:{this.props.num}</div>
+    )
+  }
+
+  componentWillUnmount() {
+    console.warn("生命周期--页面卸载 componentWillUnmount");
+  }
+}
 
 class LifeCycle extends Component {
   constructor(props) {
@@ -22,8 +33,14 @@ class LifeCycle extends Component {
     return (
       <div>
         <h1>组件的生命周期</h1>
-        <div>num:{this.state.num}</div>
-        <button onClick={this.addNum}>+1</button>
+        {
+          this.state.num>2?'组件卸载': (
+            <div>
+              <Counter num={this.state.num} />  
+              <button onClick={this.addNum}>+1</button>
+            </div>
+          )
+        }
         <button onClick={this.update}>强制更新</button>
       </div>
     );
@@ -32,7 +49,7 @@ class LifeCycle extends Component {
     console.warn("生命周期--页面加载完成 componentDidMount");
   }
   componentDidUpdate() {
-    console.warn("生命周期--页面更新完成");
+    console.warn("生命周期--页面更新完成 componentDidUpdate");
   }
 }
 
